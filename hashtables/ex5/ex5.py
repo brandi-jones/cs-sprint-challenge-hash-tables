0ddc627afd6a01e0ce882215447fdb1f2f95ad96
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-07-10 13:44:42 -0400

    working on ex5 for stretch

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,7 +7,17 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    #create queriesTable, by adding all queries to the table as keys, with values of empty arrays that will eventually contain a list of the files that contain that specific query in them
+    queriesTable = {}
+    for query in queries:
+        queriesTable[query] = []
 
+    #
+    result = []
+    for file in files:
+        foundFiles = [file for q in queries if q in file]
+        print(foundFiles)
+        
     return result
 
 
